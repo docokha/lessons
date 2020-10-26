7fa9f72f5b78ddf04e7a4b9de302ff4f56a93e44
docokha <docokha27@gmail.com>
2020-10-26 22:00:06 +1100

    test

---
+++ Lesson1/Lesson1/ViewController.swift
@@ -0,0 +1,23 @@
+//
+//  ViewController.swift
+//  Lesson1
+//
+//  Created by  Денис Дегтяренко on 26.10.2020.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    @IBAction func buttonOneTap(_ sender: Any) {
+    }
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ Lesson1/Lesson1/ViewController1.swift
@@ -0,0 +1,12 @@
+//
+//  ViewController1.swift
+//  Lesson1
+//
+//  Created by  Денис Дегтяренко on 26.10.2020.
+//
+
+import UIKit
+
+class ViewController1: UIViewController {
+    
+}
